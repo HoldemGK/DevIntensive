d1b1c01e62727d0aa661c199f030b1ad734ef0f4
HoldemGK <skrolsky@gmail.com>
2016-06-28 23:08:18 +0300

    MainActivity+CircleImage

--- app/src/main/java/com/softdesign/devintensive/ui/activities/BaseActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/BaseActivity.java
@@ -4,28 +4,48 @@ package com.softdesign.devintensive.ui.activities;
  * Created by texadmin on 27.06.2016.
  */
 
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
 public class BaseActivity extends AppCompatActivity {
     static final String TAG = ConstantManager.TAG_PREFIX + BaseActivity;
+    protected ProgressDialog mProgressDialog;
 
-    public void showProgress(){
-
+    public void showProgress() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this, R.style.custom_dialog);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.getWindow().setBackgroundDrawable(new ClipDrawable(Color.TRANSPARENT));
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
+        } else {
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
+        }
     }
 
-    public void hideProgress(){
-
+    public void hideProgress() {
+        if (mProgressDialog != null) {
+            if (mProgressDialog.isShowing()) {
+                mProgressDialog.hide();
+            }
+        }
     }
 
-    public void showError(String message,Exception error){
-
+    public void showError(String message, Exception error) {
+        showToast(message);
+        Log.d(TAG, exception.getMessage());
     }
 
-    public void showToast(String message){
-        Toast.makeText(this,)
+    public void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 
     }
 }
---
+++ app/src/main/java/com/softdesign/devintensive/utils/CircleImage.java
@@ -0,0 +1,7 @@
+package com.softdesign.devintensive.utils;
+
+/**
+ * Created by HoldemGK on 28.06.2016.
+ */
+public class CircleImage {
+}
