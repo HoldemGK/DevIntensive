1811f73d39e92b79809182347f1e546db18a6b30
HoldemGK <skrolsky@gmail.com>
2016-07-12 23:03:24 +0300

    +modules

--- app/src/main/java/com/softdesign/devintensive/data/network/interceptors/HeaderInterceptor.java
+++ app/src/main/java/com/softdesign/devintensive/data/network/interceptors/HeaderInterceptor.java
@@ -3,5 +3,30 @@ package com.softdesign.devintensive.data.network.interceptors;
 /**
  * Created by HoldemGK on 11.07.2016.
  */
-public class HeaderInterceptor {
+
+import com.softdesign.devintensive.data.managers.DataManager;
+import com.softdesign.devintensive.data.managers.PreferencesManager;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class HeaderInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        PreferencesManager pm = DataManager.getInstance().getPreferencesManager();
+
+        Request original = chain.request();
+
+        Request.Builder requestBuilder = original.newBuilder()
+                .header("X-Access-Token", pm.getAuthToken())
+                .header("Request-User-Id", pm.getUserId())
+                .header("User-Agent", "DevIntensiveApp");
+
+        Request request = requestBuilder.build();
+        return chain.proceed(request);
+    }
 }
--- app/src/main/java/com/softdesign/devintensive/data/network/req/UserLoginReq.java
+++ app/src/main/java/com/softdesign/devintensive/data/network/req/UserLoginReq.java
@@ -3,5 +3,13 @@ package com.softdesign.devintensive.data.network.req;
 /**
  * Created by HoldemGK on 11.07.2016.
  */
+
 public class UserLoginReq {
+    private String email;
+    private String password;
+
+    public UserLoginReq(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
